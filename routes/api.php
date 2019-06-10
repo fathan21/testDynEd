160cc3cb92087e906ac934a6fb62de276a30523c
muhammad fathan <mft.aang@gmail.com>
2019-06-10 13:30:15 +0700

    populer route api

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,6 @@ Route::group(['prefix' => 'v1'], function($app)
   $app->any('/setting','Api\SettingController@get');
   $app->any('/category_home','Api\SettingController@categoryHome');
   $app->any('/galery_home','Api\SettingController@galeryHome');
+
+  $app->any('/populer','Api\SettingController@populer');
 });
